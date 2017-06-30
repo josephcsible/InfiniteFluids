2a5163bd057c9d54bc0e352ca91eacccb3772cd9
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-06-29 23:51:42 -0400

    Version bump to 1.1.1

--- src/main/java/josephcsible/infinitefluids/InfiniteFluids.java
+++ src/main/java/josephcsible/infinitefluids/InfiniteFluids.java
@@ -42,7 +42,7 @@ public class InfiniteFluids
 {
 	// XXX duplication with mcmod.info and build.gradle
 	public static final String MODID = "infinitefluids";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 
 	public static Configuration config;
 	protected static boolean invertInsideNether, invertOutsideNether;
